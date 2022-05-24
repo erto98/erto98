8ace247c8d6908cf71182faffd12cc378c9f67f7
ertogrul coskuner <82051373+erto98@users.noreply.github.com>
2022-05-24 11:00:15 +0300

    bitiyorr

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -43,7 +43,7 @@ export default function Navbar() {
               </MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-              <MDBNavbarLink href="#">Basketbol</MDBNavbarLink>
+              <MDBNavbarLink href="/basketbol">Basketbol</MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
               <MDBDropdown>
@@ -55,51 +55,51 @@ export default function Navbar() {
                     <MDBDropdownLink href="/yenicami">Yenicami AK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Mağusa Türk Gücü</MDBDropdownLink>
+                    <MDBDropdownLink href="/mtg">Mağusa Türk Gücü</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Lefke TSK</MDBDropdownLink>
+                    <MDBDropdownLink href="/lefke">Lefke TSK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Gönyeli SK</MDBDropdownLink>
+                    <MDBDropdownLink href="/gonyeli">Gönyeli SK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Doğan Türk Birliği</MDBDropdownLink>
+                    <MDBDropdownLink href="/dogan">Doğan Türk Birliği</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Mesarya SK</MDBDropdownLink>
+                    <MDBDropdownLink href="/mesarya">Mesarya SK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Cihangir GSK</MDBDropdownLink>
+                    <MDBDropdownLink href="/cihangir">Cihangir GSK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Kücük Kaymaklı TSK</MDBDropdownLink>
+                    <MDBDropdownLink href="/kaymakli">Kücük Kaymaklı TSK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Göçmenköy İYKS</MDBDropdownLink>
+                    <MDBDropdownLink href="/gocmenkoy">Göçmenköy İYKS</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Türk Ocağı Limasol</MDBDropdownLink>
+                    <MDBDropdownLink href="/turkocak">Türk Ocağı Limasol</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Hamitköy ŞHSK</MDBDropdownLink>
+                    <MDBDropdownLink href="/hamitkoy">Hamitköy ŞHSK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Baf Ülkü Yurdu</MDBDropdownLink>
+                    <MDBDropdownLink href="/baf">Baf Ülkü Yurdu</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
                     <MDBDropdownLink href="/binatli">Binatlı YSK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Girne Halk Evi</MDBDropdownLink>
+                    <MDBDropdownLink href="/girnehalk">Girne Halk Evi</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>
+                    <MDBDropdownLink href="/alsancak">
                       Merit Alsancak Yeşilova SK
                     </MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Yonpaş Dumlupınar TSK</MDBDropdownLink>
+                    <MDBDropdownLink href="/dumlupinar">Yonpaş Dumlupınar TSK</MDBDropdownLink>
                   </MDBDropdownItem>
                 </MDBDropdownMenu>
               </MDBDropdown>
@@ -111,52 +111,52 @@ export default function Navbar() {
                 </MDBDropdownToggle>
                 <MDBDropdownMenu>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Çetinkaya TSK</MDBDropdownLink>
+                    <MDBDropdownLink href="/cetinkaya">Çetinkaya TSK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>China Bazaar Gençlik Gücü</MDBDropdownLink>
+                    <MDBDropdownLink href="/genclikgucu">China Bazaar Gençlik Gücü</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Miracle Değirmenlik SK</MDBDropdownLink>
+                    <MDBDropdownLink href="/degirmenlik">Miracle Değirmenlik SK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>L. Gençler Birliği SK</MDBDropdownLink>
+                    <MDBDropdownLink href="/genclerbirligi">L. Gençler Birliği SK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Karşıyaka ASK</MDBDropdownLink>
+                    <MDBDropdownLink href="/karsiyaka">Karşıyaka ASK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>SFC Dörtyol SK</MDBDropdownLink>
+                    <MDBDropdownLink href="/dortyol">SFC Dörtyol SK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Esentepe KKSK</MDBDropdownLink>
+                    <MDBDropdownLink href="/esentepe">Esentepe KKSK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Yeniboğaziçi DSK</MDBDropdownLink>
+                    <MDBDropdownLink href="/yenibogazici">Yeniboğaziçi DSK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Düzkaya KOSK</MDBDropdownLink>
+                    <MDBDropdownLink href="/duzkaya">Düzkaya KOSK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Mormenekşe GBSK</MDBDropdownLink>
+                    <MDBDropdownLink href="/mormenekse">Mormenekşe GBSK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Maraş GSK</MDBDropdownLink>
+                    <MDBDropdownLink href="/maras">Maraş GSK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>İncirli SK</MDBDropdownLink>
+                    <MDBDropdownLink href="incirli">İncirli SK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Yalova SK</MDBDropdownLink>
+                    <MDBDropdownLink href="/yalova">Yalova SK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Bostancı Bağcıl SK</MDBDropdownLink>
+                    <MDBDropdownLink href="/bostanci">Bostancı Bağcıl SK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Çanakkale TSK</MDBDropdownLink>
+                    <MDBDropdownLink href="/canakkale">Çanakkale TSK</MDBDropdownLink>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
-                    <MDBDropdownLink>Görneç KSK</MDBDropdownLink>
+                    <MDBDropdownLink href="/gornec">Görneç KSK</MDBDropdownLink>
                   </MDBDropdownItem>
                 </MDBDropdownMenu>
               </MDBDropdown>
